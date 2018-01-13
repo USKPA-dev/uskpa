d772096dd4e45287556475db6c868d719bdc3295
Vraj Mohan <radhakrishnan.vrajmohan@gsa.gov>
2018-01-12 18:20:10 -0800

    Use dj_database_url for database connection

--- uskpa/settings.py
+++ uskpa/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
+import dj_database_url
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -73,16 +74,8 @@ WSGI_APPLICATION = 'uskpa.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'kpc',
-        'USER': 'kpc',
-        'PASSWORD': 'password',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
