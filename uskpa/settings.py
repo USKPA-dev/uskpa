4132c7c025fe3cb7bb1c01f6f03c6f59a26c5032
Vraj Mohan <radhakrishnan.vrajmohan@gsa.gov>
2018-01-12 17:51:59 -0800

    Set database to Postgres

--- uskpa/settings.py
+++ uskpa/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'uskpa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kpc',
+        'USER': 'kpc',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
