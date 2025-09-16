4168729114b8f05e035e2b076f58f7e86d62666c
Erich Jacobs <ejacobs@jewelersboard.com>
2025-09-16 14:28:55 -0400

    Force UTC timezone on connection via signal

--- uskpa/settings.py
+++ uskpa/settings.py
@@ -285,3 +285,13 @@ MULTIPLE_ORIGIN_COUNTRY_DISPLAY_NAME = '***'
 COUNTRIES_OVERRIDE = {
     MULTIPLE_ORIGIN_COUNTRY_CODE: MULTIPLE_ORIGIN_COUNTRY_DISPLAY_NAME,
 }
+
+from django.db.backends.signals import connection_created
+from django.dispatch import receiver
+
+@receiver(connection_created)
+def set_postgres_utc_timezone(sender, connection, **kwargs):
+    if connection.vendor == 'postgresql':
+        with connection.cursor() as cursor:
+            cursor.execute("SET TIME ZONE 'UTC';")
+            
