90e43ff41717cea7124f2da7a318a5ba30c4501c
Erich Jacobs <ejacobs@jewelersboard.com>
2025-09-16 12:59:36 -0400

    Correct Django/Postgres timezone sync issue.

--- uskpa/settings.py
+++ uskpa/settings.py
@@ -161,7 +161,7 @@ LOGOUT_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'America/New_York'
+TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 
